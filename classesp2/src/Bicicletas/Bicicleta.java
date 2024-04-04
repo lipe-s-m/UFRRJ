2f1f115ea9b02fd08d389f1e7f1f12df9d8bc9df
lipe-s-m <felipe.s.m1609@gmail.com>
2024-04-04 02:13:14 -0300

    Estudando Classes

---
+++ classesp2/src/Bicicletas/Bicicleta.java
@@ -0,0 +1,16 @@
+package Bicicletas;
+
+public class Bicicleta 
+{
+	//Atributos
+	private int velocidade;
+	private int marcha;
+	
+	//Metodos
+	void acelerar(int incremento)
+	{
+		
+	}
+	
+
+}
---
+++ classesp2/src/module-info.java
@@ -0,0 +1,2 @@
+module classesp2 {
+}
